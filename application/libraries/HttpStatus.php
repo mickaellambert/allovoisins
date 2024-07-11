15db0f16954110956e4c8eaf44ae95c687fea6fc
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-07-11 20:46:42 +0200

    Add library for HTTP status codes

---
+++ application/libraries/HttpStatus.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class HttpStatus
+{
+    const OK = 200;
+    const CREATED = 201;
+    const NO_CONTENT = 204;
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    const INTERNAL_SERVER_ERROR = 500;
+}
